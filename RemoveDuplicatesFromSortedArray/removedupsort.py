dd59cce74d0134c16ead71b70e78d37f7e9603b0
ateeq-ur-rahman <ateeqrahman354@gmail.com>
2026-02-24 16:05:16 +0530

    Remove Duplicates From Sorted array

---
+++ RemoveDuplicatesFromSortedArray/removedupsort.py
@@ -0,0 +1,29 @@
+#  c=head
+#         while c and c.next:
+#             if c.val==c.next.val:
+#                 c.next=c.next.next
+#             else:
+#                 c=c.next
+#         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+from merge_array.sort import ListNode
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        c=head
+        while c and c.next:
+            if c.val==c.next.val:
+                c.next=c.next.next
+            else:
+                c=c.next
+        return head
+
+
