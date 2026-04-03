40c2471382be30b74714ee2e656025a6f37b9fbe
ateeq-ur-rahman <ateeqrahman354@gmail.com>
2026-04-03 11:29:14 +0530

    CountingBits

--- CountingBits/countingbits.py
+++ CountingBits/countingbits.py
@@ -1,8 +1,15 @@
+# n=int(input())
+# a=[]
+# for i in range(n+1):
+#     j=bin(i).count("1")
+#     a.append(j)
+# print(a)
+
+# #Optimal Solution
+
 n=int(input())
 a=[]
-for i in range(n+1):
-    j=bin(i).count("1")
-    a.append(j)
+a.append(0)
+for i in range(1, n+1):
+    a.append(a[i//2] + (i % 2))
 print(a)
-
-#Optimal Solution
