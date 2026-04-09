b1c18cb5cf38095dc8908cc98d86e0e69c6ef49a
ateeq-ur-rahman <ateeqrahman354@gmail.com>
2026-04-09 06:20:09 +0530

    Practicee

--- Palindrome/palindrome.py
+++ Palindrome/palindrome.py
@@ -45,8 +45,87 @@
 
 
 #Reverse a string without slicing
-s=input()
-r=""
-for i in s:
-    r=i+r
-print(r)
+# s=input()
+# r=""
+# for i in s:
+#     r=i+r
+# print(r)
+
+#Two Sum
+
+# a=list(map(int,input().split()))
+# target=int(input())
+# d={}
+
+# for i,num in enumerate(a):
+#     complement=target-num
+#     if complement in d:
+#         print([d[complement],i])
+#         break
+#     d[num]=i
+
+# for k,l in enumerate(a):
+#     print(k,l)
+
+
+#Kadane Algorithm
+
+# a=list(map(int,input().split()))
+# max_sum=float('-inf')
+# cs=0
+# for i in a:
+#     cs=cs+i
+#     max_sum=max(max_sum,cs)
+#     if cs<0:
+#         cs=0
+# print(max_sum)
+
+# a=list(map(int,input().split()))
+# cs=a[0]
+# mx=a[0]
+
+# for i in range(1,len(a)):
+#     cs=max(a[i],cs+a[i])
+#     mx=max(mx,cs)
+# print(mx)
+
+# n=int(input())
+# a=0
+# b=1
+
+# for i in range(n):
+#     c=a+b
+#     print(c,end=" ")
+#     a=b
+#     b=c
+
+
+#Print all possible sub arrays of an array optimized
+
+# a=list(map(int,input().split()))
+# for i in range(len(a)):
+#     for j in range(i,len(a)):
+#         print(a[i:j+1])
+
+
+# a=list(map(int,input().split()))
+# tg=int(input())
+
+# for i in range(len(a)):
+#     cs=0
+#     for j in range(i,len(a)):
+#         cs=cs+a[j]
+#         if cs==tg:
+#             print(a[i:j+1])
+
+
+a=list(map(int,input().split()))
+tg=int(input())
+
+for i in range(len(a)):
+    cs=0
+    for j in range(i,len(a)):
+        cs=cs+a[j]
+        if cs==tg:
+            print(a[i:j+1])
+
